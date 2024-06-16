2cf99c9e77c60c9aa34b0b713504f477a19b28d3
Summer Davis <summerd@tlgcohort.com>
2024-06-16 14:45:28 -0500

    Add framework for Bartender

--- src/com/rumprogram/bartender/Bartender.java
+++ src/com/rumprogram/bartender/Bartender.java
@@ -1,4 +1,46 @@
 package com.rumprogram.bartender;
 
+import java.util.Scanner;
+
 public class Bartender {
+
+    private final Menu menu;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void execute() {
+      welcome();
+      boolean anotherDrink = true;
+
+      while (anotherDrink) {
+        offerMenu();
+        String drinkChoice = promptForDrinkChoice();
+        makeDrink();
+        updateTab();
+        collectRating();
+        anotherDrink = askIfTheyWantAnotherDrink(); // update anotherDrink
+      }
+      closeTab();
+    }
+
+/*    private boolean askIfTheyWantAnotherDrink() {
+      boolean another;
+      if (max > MAX) {
+        another = false;
+      }
+      else if (they say yes) {
+        another = true;
+      }
+      else {
+        another = false;
+      }
+      return another;
+    }*/
+
+
+
+
+
+
+
+
 }
