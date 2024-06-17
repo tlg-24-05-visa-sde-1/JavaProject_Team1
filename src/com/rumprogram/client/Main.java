aa84b19c0ea2f1dbe40becf60b18c27eaa265168
Summer Davis <summerd@tlgcohort.com>
2024-06-17 16:38:26 -0500

    Adjust main

--- src/com/rumprogram/client/Main.java
+++ src/com/rumprogram/client/Main.java
@@ -1,16 +1,12 @@
 package com.rumprogram.client;
 
-import com.rumprogram.Menu;
 import com.rumprogram.bartender.Bartender;
 
 class Main {
 
   public static void main(String[] args){
     // instantiate controller and say "go"
-//    Bartender bartender = new Bartender();
-//    bartender.execute();
-
-      Menu menu = new Menu();
-      menu.sortByPriceHighestToLowest();
+    Bartender bartender = new Bartender();
+    bartender.execute();
   }
 }
