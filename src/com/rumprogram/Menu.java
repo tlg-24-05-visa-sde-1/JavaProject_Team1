d346c17d3534408299dccbf1e07ddfe619e840f3
Amir Saunders <AmirS@tlgcohort.com>
2024-06-17 15:06:31 -0500

    added showMenu method

--- src/com/rumprogram/Menu.java
+++ src/com/rumprogram/Menu.java
@@ -1,9 +1,18 @@
 package com.rumprogram;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class Menu {
+
+
+   private static final String DRINK_MENU = "data/drink-data.csv";
 
   // properties
   private final Collection<Drink> drinkMenu = new ArrayList<Drink>();
@@ -19,6 +28,15 @@ public class Menu {
   // findMocktails()
 
   // accessor methods
+  public static void showMenu () {
+      Scanner sc = new Scanner("data/drink-data.csv");
+      sc.useDelimiter(",");
+      while (sc.hasNext()){
+
+      }
+      System.out.println(sc.hasNext());
+      sc.close();
+  }
 
 
 
--- src/com/rumprogram/client/Main.java
+++ src/com/rumprogram/client/Main.java
@@ -1,12 +1,16 @@
 package com.rumprogram.client;
 
+import com.rumprogram.Menu;
 import com.rumprogram.bartender.Bartender;
 
 class Main {
 
   public static void main(String[] args){
     // instantiate controller and say "go"
-    Bartender bartender = new Bartender();
-    bartender.execute();
+//    Bartender bartender = new Bartender();
+//    bartender.execute();
+    Menu menu = new Menu();
+    menu.showMenu();
+
   }
 }
