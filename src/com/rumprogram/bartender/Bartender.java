53c4a7eb2a2fb4607ea23ae773786c905218925a
Summer Davis <summerd@tlgcohort.com>
2024-06-17 15:04:20 -0500

    Completed makeDrink() method

--- src/com/rumprogram/bartender/Bartender.java
+++ src/com/rumprogram/bartender/Bartender.java
@@ -9,12 +9,19 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
+import java.util.Timer;
 
 public class Bartender {
 
   // static
   private static final String WELCOME_DATA = "data/welcome.txt";
+  private static final String SCREEN1 = "data/loadingScreen/screen1.txt";
+  private static final String SCREEN2 = "data/loadingScreen/screen2.txt";
+  private static final String SCREEN3 = "data/loadingScreen/screen3.txt";
+  private static final String SCREEN4 = "data/loadingScreen/screen4.txt";
 
   private final Menu menu = new Menu();
   private final Scanner scanner = new Scanner(System.in);
@@ -51,7 +58,7 @@ public class Bartender {
 
   private void welcome() {
     try {
-      String welcomeData = Files.readString(Path.of("data/welcome.txt"));
+      String welcomeData = Files.readString(Path.of(WELCOME_DATA));
       System.out.println(welcomeData);
     } catch (IOException e) {
       e.printStackTrace();
@@ -59,9 +66,33 @@ public class Bartender {
   }
 
   private void makeDrink(Drink drinkChoice) {
+    Collection<String> loadingScreens = new ArrayList<>();
+
     try {
-      String welcomeData = Files.readString(Path.of("data/welcome.txt"));
-      System.out.println(welcomeData);
+      // create paths to loading screens
+      String screen1 = Files.readString(Path.of(SCREEN1));
+      String screen2 = Files.readString(Path.of(SCREEN2));
+      String screen3 = Files.readString(Path.of(SCREEN3));
+      String screen4 = Files.readString(Path.of(SCREEN4));
+
+      // add loading screens to collection
+      loadingScreens.add(SCREEN1);
+      loadingScreens.add(SCREEN2);
+      loadingScreens.add(SCREEN3);
+      loadingScreens.add(SCREEN4);
+
+      // generate loading screen
+      for (String loadingScreen : loadingScreens) {
+        try {
+          System.out.println(loadingScreen);
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+
+      System.out.printf("Voilà! Enjoy your %s", drinkChoice.getName());
+
     } catch (IOException e) {
       e.printStackTrace();
     }
