535ffc36133600cb1a4302e09c01be60bdd8e93a
Summer Davis <summerd@tlgcohort.com>
2024-06-17 10:58:55 -0500

    Convert inner classes Tab/Drink to top level classes

--- src/com/rumprogram/Menu.java
+++ src/com/rumprogram/Menu.java
@@ -17,44 +17,4 @@ class Menu {
 
   // accessor methods
 
-  // MEMBER-LEVEL NAMED INNER CLASS
-  private class Drink implements Comparable<Drink> {
-
-    // properties
-    private final String name;
-    private final Double price;
-    private final boolean nonAlcoholic;
-    private final int id;
-    private final Double rating;
-
-
-    Drink(String name, Double price, boolean nonAlcoholic, int id, Double rating) {
-      this.name = name;
-      this.price = price;
-      this.nonAlcoholic = nonAlcoholic;
-      this.id = id;
-      this.rating = rating;
-    }
-
-    @Override
-    public int compareTo(Drink o) {
-      return 0;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    public Double getPrice() {
-      return price;
-    }
-
-    public boolean isNonAlcoholic() {
-      return nonAlcoholic;
-    }
-
-    public int getId() {
-      return id;
-    }
-  }
 }
--- src/com/rumprogram/bartender/Bartender.java
+++ src/com/rumprogram/bartender/Bartender.java
@@ -37,41 +37,4 @@ public class Bartender {
       return another;
     }*/
 
-  // MEMBER-LEVEL NAMED INNER CLASS
-  private class Tab {
-    // static properties & methods
-    private static final int MAX_DRINKS = 3;
-
-    // properties
-    private int currentDrinkCount;
-    private Double tabTotal;
-
-    // constructors
-    public Tab() {
-
-    }
-
-    // business methods
-    private void updateTab(Double drinkPrice) {
-      // add price to total
-      // increment currentDrinkCount
-    }
-
-    // accessor methods
-    public int getCurrentDrinkCount() {
-      return currentDrinkCount;
-    }
-
-    public Double getTabTotal() {
-      return tabTotal;
-    }
-  }
-
-
-
-
-
-
-
-
 }
